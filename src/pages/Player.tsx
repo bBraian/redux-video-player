f27e7c6eae5e4ccafbf80ad479fa80c0f590f837
bBraian <braianvoficial@gmail.com>
2023-08-24 20:20:13 -0300

    responsive fixes

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -25,7 +25,7 @@ export function Player() {
 
     return (
         <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
-            <div className="flex w-[1100px] flex-col gap-6">
+            <div className="flex w-[1100px] flex-col gap-6 p-6">
                 <div className="flex items-center justify-between">
 
                     <Header />
@@ -36,11 +36,11 @@ export function Player() {
                     </button>
                 </div>
                 
-                <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
+                <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow lg:pr-80 pr-0 lg:flex-row flex-col">
                     <div className="flex-1">
                         <Video />
                     </div>
-                    <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 divide-y-2 divide-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-700">
+                    <aside className="w-80 lg:absolute relative top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 divide-y-2 divide-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-700">
                         { modules && modules.map((module, index) => {
                             return (
                                 <Module moduleIndex={index} key={module.id} title={module.title} amountOfLessons={module.lessons.length} />
